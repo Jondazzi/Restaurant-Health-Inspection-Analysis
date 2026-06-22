444054d9d29bbb723aabc8e37dabd6d5e56e6f31
Jonathan Dinh <jonathanvdinh@gmail.com>
2026-06-21 23:11:35 -0700

    added sql tables + other setup

---
+++ README.md
@@ -0,0 +1,3 @@
+inspection data https://data.lacounty.gov/datasets/lacounty::environmental-health-restaurant-and-market-inspections-04-01-2023-to-03-31-2026-1/explore
+
+violation data: https://data.lacounty.gov/datasets/5eaea9f89b7549ee841da7617d3a9cba/about
---
+++ sql/schema.sql
@@ -0,0 +1,37 @@
+CREATE DATABASE la_restaurant_inspections;
+SELECT current_database();
+
+CREATE TABLE inspections (
+    activity_date DATE,
+    owner_id VARCHAR(50),
+    owner_name VARCHAR(255),
+    facility_id VARCHAR(50),
+    facility_name VARCHAR(255),
+    record_id VARCHAR(50),
+    program_name VARCHAR(255),
+    program_status VARCHAR(50),
+    program_element VARCHAR(50),
+    pe_description VARCHAR(255),
+    facility_address VARCHAR(255),
+    facility_city VARCHAR(100),
+    facility_state VARCHAR(10),
+    facility_zip VARCHAR(20),
+    service_code INT,
+    service_description VARCHAR(255),
+    score VARCHAR(50),
+    grade VARCHAR(5),
+    serial_number VARCHAR(50) PRIMARY KEY, -- Unique ID linking both tables
+    employee_id VARCHAR(50)
+);
+
+CREATE TABLE violations (
+    serial_number VARCHAR(50) REFERENCES inspections(serial_number),
+    violation_status VARCHAR(50),
+    violation_code VARCHAR(50),
+    violation_description TEXT
+);
+
+SELECT table_name 
+FROM information_schema.tables 
+WHERE table_schema = 'public';
+
